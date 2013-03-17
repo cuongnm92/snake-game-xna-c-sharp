393c2959fdc60a362b087bd3427c912846ca06a3
cuongnm92 <cuongnm92vn@gmail.com>
2013-03-17 13:19:35 +0700

    updating simple project

---
+++ SnakeGame/SnakeGame/ScreenManager/ScreenManager.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Diagnostics;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using System.Text;
+
+namespace SnakeGame.ScreenManager
+{
+    /// <summary>
+    /// The screen manager is a component which manages one or more GameScreen
+    /// instances. It maintains a stack of screens, calls their Update and Draw
+    /// methods at the appropriate times, and automatically routes input to the
+    /// topmost active screen.
+    /// </summary>
+
+    public class ScreenManager : DrawableGameComponent
+    {
+        SpriteBatch spriteBatch;
+
+        bool isInitialized;
+        bool traceEnabled;
+
+        /// <summary>
+        /// A default SpriteBatch shared by all the screens. This saves
+        /// each screen having to bother creating their own local instance.
+        /// </summary>
+        public SpriteBatch SpriteBatch
+        {
+            get { return spriteBatch; }
+        }
+    }
+}
