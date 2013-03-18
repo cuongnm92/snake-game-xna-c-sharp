5889c86ab06931deb345baf4ce5a4853ac042a2c
cuongnm92 <cuongnm92vn@gmail.com>
2013-03-18 18:06:13 +0700

    Updating Game Data

---
+++ SnakeGameData/Direction.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SnakeGameData
+{
+    /// <summary>
+    /// Cardinal and ordinal directions, primary for sprite orientation.
+    /// </summary>
+    public enum Direction
+    {
+        North,
+        NorthEast,
+        East,
+        SouthEast,
+        South,
+        SouthWest,
+        West,
+        NorthWest,
+    }
+}
